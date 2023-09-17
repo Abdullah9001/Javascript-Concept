aa0868da3b727530f828ad3275530333bd9101bf
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-09-18 01:20:05 +0600

    filet section done

--- 1/map_filter_reduce.js
+++ 1/map_filter_reduce.js
@@ -1,7 +1,15 @@
-const number = [1, 2, 3, 4];
+const number = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
 
 let num2 = number.map((value) => {
   return value + 6;
 });
 
 console.log(num2);
+
+const num3 = number.filter((value) => {
+  return value > 6;
+});
+
+console.log(num3);
