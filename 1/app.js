373bae2edcad5dac3fe061311ed9b5c8ff34bda9
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-09-17 19:56:41 +0600

    start learning javacript concept

---
+++ 1/app.js
@@ -0,0 +1,21 @@
+const arr = [1, 2, 4];
+
+function avg(a, b, c) {
+  return a + b + c / 3;
+}
+
+let a = avg(...arr);
+console.log(a);
+
+let b = [...arr];
+console.log(b);
+
+const obj1 = {
+  name: "John",
+  age: 25,
+  city: "New York",
+};
+
+const obj2 = { ...obj1, city: "Dhaka" };
+
+console.log(obj2);
